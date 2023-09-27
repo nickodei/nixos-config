b715d393074ba791ebd84496b3f092818dc2e862
Niek Deibus <nickdeibus@outlook.de>
2023-09-27 20:51:59 +0200

    chore(test): test

--- hosts/dell-xps/default.nix
+++ hosts/dell-xps/default.nix
@@ -96,7 +96,9 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
+  programs.ssh.startAgent = true;
+
   services.pipewire.enable = true;
 
   # Open ports in the firewall.
