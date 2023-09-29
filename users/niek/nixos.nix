f6e6c29a9ad7f5f031e1a7b879fd3f5f94c286ab
nickodei <nickdeibus@outlook.de>
2023-09-29 16:49:58 +0200

    sdf

--- users/niek/nixos.nix
+++ users/niek/nixos.nix
@@ -37,7 +37,7 @@
   };
 
   fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "Inconsolata" "InconsolataGo" ]; })
+    (nerdfonts.override { fonts = [ "SauceCodePro" ]; })
   ];
 
   security.polkit.enable = true;
