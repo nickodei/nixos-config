ee69a04f11d8369d96017a2485c1e093cdf079b2
nickodei <nickdeibus@outlook.de>
2023-09-30 00:23:30 +0200

    dsfds

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -8,8 +8,6 @@
                 ll = "ls -l";
                 update = "sudo nixos-rebuild switch --flake /etc/home/niek/nixos-config#work";
             };
-            histSize = 10000;
-            histFile = "~/.config/zsh/history";
             ohMyZsh = {
                 enable = true;
                 plugins = [ "git" "thefuck" ];
