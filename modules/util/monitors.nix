746928ae19422c221f60bc9968b4049c1e2b4f7e
Niek Deibus <nickdeibus@outlook.de>
2023-10-21 17:44:47 +0200

    test

--- modules/util/monitors.nix
+++ modules/util/monitors.nix
@@ -4,29 +4,6 @@ let
   inherit (lib) mkOption types;
 in
 {
-  options.available-monitors = {
-    surface-pro-8 = {
-      name = "eDP-1";
-      width = 2880;
-      height = 1920;
-      refreshRate = 60;
-      x = 0;
-      y = 0;
-      scale = 2;
-      enabled = true;
-    };
-    dell-xps-17 = {
-      name = "eDP-1";
-      width = 3840;
-      height = 2400;
-      refreshRate = 60;
-      x = 0;
-      y = 0;
-      scale = 2;
-      enabled = true;
-    };
-  };
-
   options.monitors = mkOption {
     type = types.listOf (types.submodule {
       options = {
