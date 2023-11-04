ab710432501ade1d348b0fa679abe936e11ed04e
Niek Deibus <nickdeibus@outlook.de>
2023-11-04 22:56:49 +0100

    updated nvim

--- modules/editor/nvim/default.nix
+++ modules/editor/nvim/default.nix
@@ -11,98 +11,93 @@ let
 in
 {
   options.modules.nvim = { enable = lib.mkEnableOption "nvim"; };
-  programs.neovim = {
-    enable = true;
 
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
 
-    extraPackages = with pkgs; [
-      # telescope
-      ripgrep
-      fd
+  config = lib.mkIf cfg.enable
+    {
+      programs.neovim = {
+        enable = true;
 
-      # lsp
-      lua-language-server
-      rnix-lsp
-      omnisharp-roslyn
-      clang-tools_16
-    ];
+        viAlias = true;
+        vimAlias = true;
+        vimdiffAlias = true;
 
-    plugins = with pkgs.vimPlugins; [
-      {
-        plugin = telescope-nvim;
-        type = "lua";
-        config = ''
-          local builtin = require('telescope.builtin')
-          vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-          vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-          vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-          vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        extraPackages = with pkgs; [
+          # telescope
+          ripgrep
+          fd
+
+          # lsp
+          lua-language-server
+          rnix-lsp
+          omnisharp-roslyn
+          clang-tools_16
+        ];
+
+        plugins = with pkgs.vimPlugins; [
+          {
+            plugin = telescope-nvim;
+            type = "lua";
+            config = ''
+              local builtin = require('telescope.builtin')
+              vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+              vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+              vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+              vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            '';
+          }
+          {
+            plugin = nvim-lspconfig;
+            type = "lua";
+            config = lsp_config;
+          }
+          {
+            plugin = nvim-cmp;
+            type = "lua";
+            config = builtins.readFile ./plugins/cmp.lua;
+          }
+          {
+            plugin = lualine-nvim;
+            type = "lua";
+            config = ''
+              require("lualine").setup({
+                icons_enabled = true,
+                theme = 'onedark',
+              })
+            '';
+          }
+          {
+            plugin = comment-nvim;
+            type = "lua";
+            config = ''require("Comment").setup {}'';
+          }
+          {
+            plugin = auto-save-nvim;
+            type = "lua";
+            config = ''require("auto-save").setup {}'';
+          }
+          {
+            plugin = lsp-format-nvim;
+            type = "lua";
+            config = ''require("lsp-format").setup {}'';
+          }
+          cmp_luasnip
+          cmp-nvim-lsp
+          luasnip
+          vim-nix
+          customPlugins.omnisharp-vim
+          nvim-treesitter.withAllGrammars
+          {
+            plugin = sonokai;
+            config = "colorscheme sonokai";
+          }
+        ];
+
+        extraLuaConfig = ''
+          ${builtins.readFile ./options.lua}
         '';
-      }
-      {
-        plugin = nvim-lspconfig;
-        type = "lua";
-        config = lsp_config;
-      }
-      {
-        plugin = nvim-cmp;
-        type = "lua";
-        config = builtins.readFile ./plugins/cmp.lua;
-      }
-      {
-        plugin = lualine-nvim;
-        type = "lua";
-        config = ''
-          require("lualine").setup({
-            icons_enabled = true,
-            theme = 'onedark',
-          })
-        '';
-      }
-      {
-        plugin = comment-nvim;
-        type = "lua";
-        config = ''require("Comment").setup {}'';
-      }
-      {
-        plugin = auto-save-nvim;
-        type = "lua";
-        config = ''require("auto-save").setup {}'';
-      }
-      {
-        plugin = lsp-format-nvim;
-        type = "lua";
-        config = ''require("lsp-format").setup {}'';
-      }
-      cmp_luasnip
-      cmp-nvim-lsp
-      luasnip
-      vim-nix
-      customPlugins.omnisharp-vim
-      nvim-treesitter.withAllGrammars
-      {
-        plugin = sonokai;
-        config = "colorscheme sonokai";
-      }
-    ];
-
-    extraLuaConfig = ''
-      ${builtins.readFile ./options.lua}
-    '';
-  };
-};
+      };
+    };
 }
-
-
-
-
-
-
-
-
-
 
 
