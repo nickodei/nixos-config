835fddedea4df6f3542850b366ea9c903cfc9270
Niek Deibus <nickdeibus@outlook.de>
2023-11-04 12:56:40 +0100

    updated nvim

--- modules/editor/nvim/default.nix
+++ modules/editor/nvim/default.nix
@@ -6,35 +6,39 @@ in
 {
   options.modules.nvim = { enable = lib.mkEnableOption "nvim"; };
 
-  config = lib.mkIf cfg.enable
-    {
-      programs.neovim = {
-        enable = true;
+  config = lib.mkIf cfg.enable {
+    programs.neovim = {
+      enable = true;
 
-        viAlias = true;
-        vimAlias = true;
-        vimdiffAlias = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
 
-        extraPackages = with pkgs; [
-          ripgrep
-          fd
-        ];
+      extraPackages = with pkgs; [
+        ripgrep
+        fd
+      ];
 
-        plugins = with pkgs.vimPlugins; [
-          {
-            plugin = telescope-nvim;
-            type = "lua";
-            config = ''
-              nmap("<leader>ff", ":Telescope find_files<cr>")
-              nmap("<leader>fg", ":Telescope live_grep<cr>")
-              nmap("<leader>fb", ":Telescope buffers<cr>")
-              nmap("<leader>fh", ":Telescope help_tags<cr>")
-            '';
-          }
-          nvim-treesitter.withAllGrammars
-        ];
-      };
+      plugins = with pkgs.vimPlugins; [
+        {
+          plugin = telescope-nvim;
+          type = "lua";
+          config = ''
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+          '';
+        }
+        nvim-treesitter.withAllGrammars
+      ];
+
+      extraLuaConfig = ''
+        ${builtins.readFile ./options.lua}
+      '';
     };
+  };
 }
 
 
--- users/main/home.nix
+++ users/main/home.nix
@@ -2,31 +2,16 @@
 { config, lib, pkgs, inputs, host, user, ... }:
 
 let
-  available-monitors = {
-    surface-pro-8 = {
-      name = "eDP-1";
-      width = 2880;
-      height = 1920;
-      refreshRate = 60;
-      x = 0;
-      y = 0;
-      scale = 2;
-      enabled = true;
-    };
-    dell-xps-17 = {
-      name = "eDP-1";
-      width = 3840;
-      height = 2400;
-      refreshRate = 60;
-      x = 0;
-      y = 0;
-      scale = 2;
-      enabled = true;
-    };
-  };
+  available-monitors = import ./../../modules/desktop/wayland/monitors.nix;
+  monitors = (
+    if (host == "dell-xps-17") then [ available-monitors.dell-xps-17 ]
+    else if (host == "surface-pro") then [ available-monitors.surface-pro-8 ]
+    else [ ]
+  ) ++ [ available-monitors.view-sonic-32 ];
 in { 
   imports = [ 
     ../../modules
+    ../../modules/programs/imports.nix
     inputs.nix-colors.homeManagerModules.default
   ];
 
@@ -40,24 +25,24 @@ in {
     git.enable = true;
     vscodium.enable = true;
 
-    hyprland = {
-      enable = true;
-      hidpi = true;
+    spotify.enable = true;
+    obsidian.enable = true;
+    blueman-applet.enable = true;
+
+    wayland = {
+      hyprland = {
+        enable = true;
+        hidpi = true;
+        monitors = monitors;
+      };
+      wlogout.enable = true;
     };
-    
+
     hyprpaper.enable = true;
     waybar.enable = true;
 
     # Development
     direnv.enable = true;
-  };
-    
-    monitors = if (host == "dell-xps-17") 
-    then [available-monitors.dell-xps-17] 
-    else [available-monitors.surface-pro-8];
-
-  programs.wlogout = {
-    enable = true;
   };
 
   # Home - Default Settings
