f7994cd383d6934c37b23c4ac7437ef64c99e427
Niek Deibus <nickdeibus@outlook.de>
2023-11-12 23:22:40 +0100

    changing stuff

--- modules/desktop/wayland/hyprland/default.nix
+++ modules/desktop/wayland/hyprland/default.nix
@@ -101,8 +101,8 @@ in {
         inputs.hyprgrass.packages.${pkgs.system}.default
       ];
       extraConfig = ''
-          # Monitors
-          ${builtins.concatStringsSep "\n" (map (
+                # Monitors
+                ${builtins.concatStringsSep "\n" (map (
           m: let
             resolution = "${toString m.width}x${toString m.height}@${toString m.refreshRate}";
             position = "${toString m.x}x${toString m.y}";
@@ -113,12 +113,12 @@ in {
           }"
         ) (cfg.monitors))}
 
-          # Startup Applications
-          exec-once = waybar & hyprpaper & mako
-          exec-once = nm-applet --indicator
-          exec-once = blueman-applet
+                # Startup Applications
+                exec-once = waybar & hyprpaper & mako
+                exec-once = nm-applet --indicator
+                exec-once = blueman-applet
 
-          ${
+                ${
           if cfg.hidpi
           then ''
             # Fix HiDPI XWayland windows
@@ -127,165 +127,166 @@ in {
           else ""
         }
 
-          # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
-          input {
-              kb_layout = de
-              follow_mouse = 1
-              touchpad {
-                  natural_scroll = true
+                # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
+                input {
+                    kb_layout = de
+                    follow_mouse = 1
+                    touchpad {
+                        natural_scroll = true
+                    }
+                    sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+                }
+
+                general {
+                    # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+                    gaps_in = 4
+                    gaps_out = 8
+                    border_size = 2
+                    col.active_border = rgba(595cffee) rgba(c6f8ffee) -45deg #c6f8ffee
+                    col.inactive_border = rgba(595959aa)
+
+                    layout = dwindle
+                }
+
+              plugin {
+                touch_gestures {
+                  # default sensitivity is probably too low on tablet screens,
+                  # I recommend turning it up to 4.0
+                  sensitivity = 4.0
+                  workspace_swipe_fingers = 3
+                }
               }
-              sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
-          }
 
-          general {
-              # See https://wiki.hyprland.org/Configuring/Variables/ for more
+              decoration {
+                  # See https://wiki.hyprland.org/Configuring/Variables/ for more
 
-              gaps_in = 4
-              gaps_out = 8
-              border_size = 2
-              col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-              col.inactive_border = rgba(595959aa)
+                  rounding =5
 
-              layout = dwindle
-          }
+                  blur {
+                      enabled = true
+                      size = 3
+                      passes = 1
+                  }
 
-        plugin {
-          touch_gestures {
-            # default sensitivity is probably too low on tablet screens,
-            # I recommend turning it up to 4.0
-            sensitivity = 4.0
-            workspace_swipe_fingers = 3
-          }
-        }
+                  drop_shadow = true
+                  shadow_range = 4
+                  shadow_render_power = 3
+                  col.shadow = rgba(1a1a1aee)
+              }
 
-        decoration {
-            # See https://wiki.hyprland.org/Configuring/Variables/ for more
+              animations {
+                  enabled = true
 
-            rounding =5
+                  # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
 
-            blur {
-                enabled = true
-                size = 3
-                passes = 1
-            }
+                  bezier = myBezier, 0.05, 0.9, 0.1, 1.05
 
-            drop_shadow = true
-            shadow_range = 4
-            shadow_render_power = 3
-            col.shadow = rgba(1a1a1aee)
-        }
+                  animation = windows, 1, 7, myBezier
+                  animation = windowsOut, 1, 7, default, popin 80%
+                  animation = border, 1, 10, default
+                  animation = borderangle, 1, 8, default
+                  animation = fade, 1, 7, default
+                  animation = workspaces, 1, 6, default
+              }
 
-        animations {
-            enabled = true
+              dwindle {
+                  # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+                  pseudotile = true # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+                  preserve_split = true # you probably want this
+              }
 
-            # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+              master {
+                  # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+                  new_is_master = true
+              }
 
-            bezier = myBezier, 0.05, 0.9, 0.1, 1.05
+              gestures {
+                  # See https://wiki.hyprland.org/Configuring/Variables/ for more
+                  workspace_swipe = true
+                  # e_swipe_cancel_ratio = 0.15
+              }
 
-            animation = windows, 1, 7, myBezier
-            animation = windowsOut, 1, 7, default, popin 80%
-            animation = border, 1, 10, default
-            animation = borderangle, 1, 8, default
-            animation = fade, 1, 7, default
-            animation = workspaces, 1, 6, default
-        }
+              # Example per-device config
+              # See https://wiki.hyprland.org/Configuring/Keywords/#per-device-input-configs for more
+              device:epic-mouse-v1 {
+                  sensitivity = -0.5
+              }
 
-        dwindle {
-            # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-            pseudotile = true # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-            preserve_split = true # you probably want this
-        }
-
-        master {
-            # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
-            new_is_master = true
-        }
-
-        gestures {
-            # See https://wiki.hyprland.org/Configuring/Variables/ for more
-            workspace_swipe = true
-            # e_swipe_cancel_ratio = 0.15
-        }
-
-        # Example per-device config
-        # See https://wiki.hyprland.org/Configuring/Keywords/#per-device-input-configs for more
-        device:epic-mouse-v1 {
-            sensitivity = -0.5
-        }
-
-        # Example windowrule v1
-        windowrule = float, ^(.blueman-manager-wrapped)$
-        # Example windowrule v2
-        # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
-        # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
+              # Example windowrule v1
+              windowrule = float, ^(.blueman-manager-wrapped)$
+        windowrule = opacity 0.9,^(kitty)$
+              # Example windowrule v2
+              # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
+              # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
 
 
-        # See https://wiki.hyprland.org/Configuring/Keywords/ for more
-        $mainMod = SUPER
+              # See https://wiki.hyprland.org/Configuring/Keywords/ for more
+              $mainMod = SUPER
 
-        # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-        bind = $mainMod, K, exec, kitty
-        bind = $mainMod, Q, killactive,
-        bind = $mainMod, C, exec, code
-        bind = $mainMod, O, exec, obsidian
-        bind = $mainMod, S, exec, spotify
-        bind = $mainMod, L, exec, swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2
-        bind = $mainMod, M, exit,
-        bind = $mainMod, E, exec, thunar
-        bind = $mainMod, V, togglefloating,
-        bind = $mainMod, R, exec, wofi --show drun
-        bind = $mainMod, P, pseudo, # dwindle
-        bind = $mainMod, J, togglesplit, # dwindle
-        bind = $mainMod, B, exec, firefox
-        bind = $mainMod, R, exec, rofi -show drun
+              # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
+              bind = $mainMod, K, exec, kitty
+              bind = $mainMod, Q, killactive,
+              bind = $mainMod, C, exec, code
+              bind = $mainMod, O, exec, obsidian
+              bind = $mainMod, S, exec, spotify
+              bind = $mainMod, L, exec, swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2
+              bind = $mainMod, M, exit,
+              bind = $mainMod, E, exec, thunar
+              bind = $mainMod, V, togglefloating,
+              bind = $mainMod, R, exec, wofi --show drun
+              bind = $mainMod, P, pseudo, # dwindle
+              bind = $mainMod, J, togglesplit, # dwindle
+              bind = $mainMod, B, exec, firefox
+              bind = $mainMod, R, exec, rofi -show drun
 
-        # Fn-Keys
-        binde  = , XF86AudioRaiseVolume, exec, pamixer -i 5
-        binde  = , XF86AudioLowerVolume, exec, pamixer -d 5
-        bindle = , XF86AudioMute, exec, pamixer --toggle-mute
-        binde  = , xf86MonBrightnessUp, exec, brightnessctl set +10%
-        binde  = , xf86MonBrightnessDown, exec, brightnessctl set 10%-
+              # Fn-Keys
+              binde  = , XF86AudioRaiseVolume, exec, pamixer -i 5
+              binde  = , XF86AudioLowerVolume, exec, pamixer -d 5
+              bindle = , XF86AudioMute, exec, pamixer --toggle-mute
+              binde  = , xf86MonBrightnessUp, exec, brightnessctl set +10%
+              binde  = , xf86MonBrightnessDown, exec, brightnessctl set 10%-
 
 
-         exec, pamixer -d 5
+               exec, pamixer -d 5
 
-        # Move focus with mainMod + arrow keys
-        bind = $mainMod, left, movefocus, l
-        bind = $mainMod, right, movefocus, r
-        bind = $mainMod, up, movefocus, u
-        bind = $mainMod, down, movefocus, d
+              # Move focus with mainMod + arrow keys
+              bind = $mainMod, left, movefocus, l
+              bind = $mainMod, right, movefocus, r
+              bind = $mainMod, up, movefocus, u
+              bind = $mainMod, down, movefocus, d
 
-        # Switch workspaces with mainMod + [0-9]
-        bind = $mainMod, 1, workspace, 1
-        bind = $mainMod, 2, workspace, 2
-        bind = $mainMod, 3, workspace, 3
-        bind = $mainMod, 4, workspace, 4
-        bind = $mainMod, 5, workspace, 5
-        bind = $mainMod, 6, workspace, 6
-        bind = $mainMod, 7, workspace, 7
-        bind = $mainMod, 8, workspace, 8
-        bind = $mainMod, 9, workspace, 9
-        bind = $mainMod, 0, workspace, 10
+              # Switch workspaces with mainMod + [0-9]
+              bind = $mainMod, 1, workspace, 1
+              bind = $mainMod, 2, workspace, 2
+              bind = $mainMod, 3, workspace, 3
+              bind = $mainMod, 4, workspace, 4
+              bind = $mainMod, 5, workspace, 5
+              bind = $mainMod, 6, workspace, 6
+              bind = $mainMod, 7, workspace, 7
+              bind = $mainMod, 8, workspace, 8
+              bind = $mainMod, 9, workspace, 9
+              bind = $mainMod, 0, workspace, 10
 
-        # Move active window to a workspace with mainMod + SHIFT + [0-9]
-        bind = $mainMod SHIFT, 1, movetoworkspace, 1
-        bind = $mainMod SHIFT, 2, movetoworkspace, 2
-        bind = $mainMod SHIFT, 3, movetoworkspace, 3
-        bind = $mainMod SHIFT, 4, movetoworkspace, 4
-        bind = $mainMod SHIFT, 5, movetoworkspace, 5
-        bind = $mainMod SHIFT, 6, movetoworkspace, 6
-        bind = $mainMod SHIFT, 7, movetoworkspace, 7
-        bind = $mainMod SHIFT, 8, movetoworkspace, 8
-        bind = $mainMod SHIFT, 9, movetoworkspace, 9
-        bind = $mainMod SHIFT, 0, movetoworkspace, 10
+              # Move active window to a workspace with mainMod + SHIFT + [0-9]
+              bind = $mainMod SHIFT, 1, movetoworkspace, 1
+              bind = $mainMod SHIFT, 2, movetoworkspace, 2
+              bind = $mainMod SHIFT, 3, movetoworkspace, 3
+              bind = $mainMod SHIFT, 4, movetoworkspace, 4
+              bind = $mainMod SHIFT, 5, movetoworkspace, 5
+              bind = $mainMod SHIFT, 6, movetoworkspace, 6
+              bind = $mainMod SHIFT, 7, movetoworkspace, 7
+              bind = $mainMod SHIFT, 8, movetoworkspace, 8
+              bind = $mainMod SHIFT, 9, movetoworkspace, 9
+              bind = $mainMod SHIFT, 0, movetoworkspace, 10
 
-        # Scroll through existing workspaces with mainMod + scroll
-        bind = $mainMod, mouse_down, workspace, e+1
-        bind = $mainMod, mouse_up, workspace, e-1
+              # Scroll through existing workspaces with mainMod + scroll
+              bind = $mainMod, mouse_down, workspace, e+1
+              bind = $mainMod, mouse_up, workspace, e-1
 
-        # Move/resize windows with mainMod + LMB/RMB and dragging
-        bindm = $mainMod, mouse:272, movewindow
-        bindm = $mainMod, mouse:273, resizewindow
+              # Move/resize windows with mainMod + LMB/RMB and dragging
+              bindm = $mainMod, mouse:272, movewindow
+              bindm = $mainMod, mouse:273, resizewindow
       '';
     };
 
--- modules/terminal/kitty/default.nix
+++ modules/terminal/kitty/default.nix
@@ -1,9 +1,12 @@
-{ inputs, pkgs, lib, config, ... }:
-
-let
-  cfg = config.modules.kitty;
-in
 {
+  inputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.modules.kitty;
+in {
   options.modules.kitty = {
     enable = lib.mkEnableOption "fish";
   };
@@ -11,54 +14,103 @@ in
   config = lib.mkIf cfg.enable {
     programs.kitty = {
       enable = true;
+      extraConfig = ''
+        background #2b2d3a
+        foreground #e1e3e4
+
+        selection_background #3a3e4e
+        selection_foreground #e1e3e4
+
+        cursor #e1e3e4
+        cursor_text_color background
+
+        active_tab_background #2b2d37
+        active_tab_foreground #e1e3e4
+        active_tab_font_style bold
+        inactive_tab_background #2b2d37
+        inactive_tab_foreground #e1e3e4
+        inactive_tab_font_style normal
+
+        # Black
+        color0 #181a1c
+        color8 #7e8294
+
+        # Red
+        color1 #fb617e
+        color9 #fb617e
+
+        # Green
+        color2 #9ed06c
+        color10 #9ed06c
+
+        # Yellow
+        color3 #f0c362
+        color11 #f0c362
+
+        # Blue
+        color4 #6dcae8
+        color12 #6dcae8
+
+        # Magenta
+        color5 #bb97ee
+        color13 #bb97ee
+
+        # Cyan
+        color6 #f89860
+        color7 #f89860
+
+        # White
+        color7 #e1e3e4
+        color15 #e1e3e4
+      '';
       settings = with config.colorScheme.colors; {
         scrollback_lines = 10000;
         enable_audio_bell = false;
         window_padding_width = 5;
 
-        background = "#${base00}";
-        foreground = "#${base05}";
-        selection_background = "#${base05}";
-        selection_foreground = "#${base00}";
-        url_color = "#${base04}";
-        cursor = "#${base05}";
-        active_border_color = "#${base03}";
-        inactive_border_color = "#${base01}";
-        active_tab_background = "#${base00}";
-        active_tab_foreground = "#${base05}";
-        inactive_tab_background = "#${base01}";
-        inactive_tab_foreground = "#${base04}";
-        tab_bar_background = "#${base01}";
-
-        # normal
-        color0 = "#${base00}";
-        color1 = "#${base08}";
-        color2 = "#${base0B}";
-        color3 = "#${base0A}";
-        color4 = "#${base0D}";
-        color5 = "#${base0E}";
-        color6 = "#${base0C}";
-        color7 = "#${base05}";
-
-        # bright
-        color8 = "#${base03}";
-        color9 = "#${base08}";
-        color10 = "#${base0B}";
-        color11 = "#${base0A}";
-        color12 = "#${base0D}";
-        color13 = "#${base0E}";
-        color14 = "#${base0C}";
-        color15 = "#${base07}";
+        # background = "#${base00}";
+        # foreground = "#${base05}";
+        # selection_background = "#${base05}";
+        # selection_foreground = "#${base00}";
+        # url_color = "#${base04}";
+        # cursor = "#${base05}";
+        # active_border_color = "#${base03}";
+        # inactive_border_color = "#${base01}";
+        # active_tab_background = "#${base00}";
+        # active_tab_foreground = "#${base05}";
+        # inactive_tab_background = "#${base01}";
+        # inactive_tab_foreground = "#${base04}";
+        # tab_bar_background = "#${base01}";
+        #
+        # # normal
+        # color0 = "#${base00}";
+        # color1 = "#${base08}";
+        # color2 = "#${base0B}";
+        # color3 = "#${base0A}";
+        # color4 = "#${base0D}";
+        # color5 = "#${base0E}";
+        # color6 = "#${base0C}";
+        # color7 = "#${base05}";
+        #
+        # # bright
+        # color8 = "#${base03}";
+        # color9 = "#${base08}";
+        # color10 = "#${base0B}";
+        # color11 = "#${base0A}";
+        # color12 = "#${base0D}";
+        # color13 = "#${base0E}";
+        # color14 = "#${base0C}";
+        # color15 = "#${base07}";
 
         # extended base16 colors
-        color16 = "#${base09}";
-        color17 = "#${base0F}";
-        color18 = "#${base01}";
-        color19 = "#${base02}";
-        color20 = "#${base04}";
-        color21 = "#${base06}";
+        # color16 = "#${base09}";
+        # color17 = "#${base0F}";
+        # color18 = "#${base01}";
+        # color19 = "#${base02}";
+        # color20 = "#${base04}";
+        # color21 = "#${base06}";
       };
-      theme = "Gruvbox Material Dark Medium";
+      # theme = "Gruvbox Material Dark Medium";
       font.name = "Fira Code"; #"SauceCodePro NFM";
       font.size = 14;
       shellIntegration.mode = "enabled";
@@ -66,4 +118,3 @@ in
     };
   };
 }
-
